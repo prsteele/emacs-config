c22f649bb18378e67cc5ba21a71cf9990c2b76b7
Patrick Steele <prs233@cornell.edu>
2013-11-05 13:34:17 -0500

    Disallowed completion by RET, allowing early termination of words that aren't in the completion list

--- prsteele-auto-complete.el
+++ prsteele-auto-complete.el
@@ -2,3 +2,13 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict")
 (require 'auto-complete-config)
 (ac-config-default)
+
+;; Don't allow completion by RET
+(define-key ac-completing-map [return] (lambda ()
+					 "Don't allow completion by RET"
+					 (interactive)
+					 (progn
+					   (ac-stop)
+					   (newline-and-indent))))
+
+
