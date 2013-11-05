e74a4f8f7b0ac1b073ffaaefcec3b09cda5b7e67
Patrick Steele <prs233@cornell.edu>
2013-11-05 13:33:44 -0500

    Made go-mode optional; fails gracefully if files are not present

--- prsteele-go.el
+++ prsteele-go.el
@@ -3,14 +3,15 @@
 ;; GO
 ;;
 (add-to-list 'load-path "~/lib/go/misc/emacs")
-(require 'go-mode-load)
-(add-hook 'go-mode-hook
-	  '(lambda ()
-	     (setq default-tab-width 2)))
-(add-hook 'before-save-hook #'gofmt-before-save)
-(add-hook 'go-mode-hook 
-	  (lambda () 
-	    (progn
-	      (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-	      (local-set-key (kbd "C-c C-a") 'go-import-add)
-	      (local-set-key (kbd "C-c C-i") 'go-goto-imports))))
+(if (require 'go-mode-load nil t)
+    (progn
+      (add-hook 'go-mode-hook
+		'(lambda ()
+		   (setq default-tab-width 2)))
+      (add-hook 'before-save-hook #'gofmt-before-save)
+      (add-hook 'go-mode-hook 
+		(lambda () 
+		  (progn
+		    (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+		    (local-set-key (kbd "C-c C-a") 'go-import-add)
+		    (local-set-key (kbd "C-c C-i") 'go-goto-imports))))))
