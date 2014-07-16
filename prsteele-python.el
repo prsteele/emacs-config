fbf6c192ec9ae71f7a8a65aff788898ccffbb50d
Patrick Steele <steele.pat@gmail.com>
2014-07-15 19:40:40 -0700

    Modified prsteele-python.el to use python-mode.el.

--- prsteele-python.el
+++ prsteele-python.el
@@ -3,21 +3,6 @@
 ;; Python
 ;;
 
-(add-hook 'python-mode-hook 
-	  '(lambda ()
-	     (local-set-key (kbd "RET") 'newline-and-indent)))
-
-;; Cython mode
-(add-to-list 'auto-mode-alist '("\\.pyx\\'" . cython-mode))
-
-(define-derived-mode cython-mode python-mode "Cython"
-  (font-lock-add-keywords
-   nil
-   `((,(concat "\\<\\(NULL"
-               "\\|c\\(def\\|har\\|typedef\\)"
-               "\\|e\\(num\\|xtern\\)"
-               "\\|float"
-               "\\|in\\(clude\\|t\\)"
-               "\\|object\\|public\\|struct\\|type\\|union\\|void"
-               "\\)\\>")
-      1 font-lock-keyword-face t))))
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
