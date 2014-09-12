2ddbbf5f68d4a2775f12b09c1a066ba01cd0b983
Patrick Steele <prs233@cornell.edu>
2014-09-12 10:26:31 -0400

    Working on getting Python support for company

--- prsteele-javascript.el
+++
@@ -1,8 +0,0 @@
-(require 'color-theme-buffer-local)
-
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
-(add-hook 'js-mode-hook
-          (lambda () 
-            (color-theme-buffer-local 
-             'color-theme-taming-mr-arneson (current-buffer))))
