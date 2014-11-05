f05c327247122a1c8ddeca27b299bd3bdd11a1d9
Patrick Steele <prs233@cornell.edu>
2014-11-05 14:31:05 -0500

    Added support for org-mode.

---
+++ prsteele-org.el
@@ -0,0 +1,26 @@
+;; Configuration file for org-mode
+
+(require 'org-install)
+(require 'ob-tangle)
+(require 'ob-latex)
+(require 'org-latex)
+
+(setq org-src-fontify-natively t)
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
+
+(define-key global-map (kbd "C-c c") 'org-capture)
+
+(add-hook 'org-mode-hook 'auto-fill-mode)
+(add-hook 'org-mode-hook 'flyspell-mode)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/todo.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+         "* %?\nEntered on %U\n  %i\n  %a")))
+
+
+
