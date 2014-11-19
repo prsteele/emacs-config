f849ef3e66edb11257ca9e57b5781c3f34549be7
Patrick Steele <prs233@cornell.edu>
2014-11-19 14:49:10 -0500

    Cleaned up the init.el file a bit.
    Removed some files for modes I don't use anymore.

--- prsteele-company.el
+++ prsteele-company.el
@@ -1,8 +1,7 @@
 (require 'company)
+
 (add-hook 'after-init-hook 'global-company-mode)
 
 (add-to-list 'company-backends 'company-c-headers)
 
 (setq company-idle-delay .2)
-
-;(add-to-list 'company-c-headers-path-system "/usr/include/c++/4.8/")
