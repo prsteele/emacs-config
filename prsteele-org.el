3aa0f7a89e1452933ca7c5e0a23c0d16997497a4
Patrick Steele <prs233@cornell.edu>
2015-08-21 14:39:29 -0400

    Updated my Org config.

--- prsteele-org.el
+++ prsteele-org.el
@@ -13,17 +13,24 @@
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c b") 'org-iswitchb)
-
 (define-key global-map (kbd "C-c c") 'org-capture)
 
 (add-hook 'org-mode-hook 'auto-fill-mode)
 (add-hook 'org-mode-hook 'flyspell-mode)
 
+;; Add timestamps to completed TODOs
+(setq org-log-done 'time)
+
+(setq org-agenda-files (list "~/org/agenda.org"
+                             "~/org/todo.org"))
+
+
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
+
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/org/todo.org" "Tasks")
-         "* TODO %?\n  %i\n  %a")
+         "* TODO %?\n\nCreated at %U")
         ("j" "Journal" entry (file+datetree "~/org/journal.org")
-         "* %?\nEntered on %U\n  %i\n  %a")))
-
-
-
+         "* %?\nEntered on %U\n  %i\n  %a")
+        ("r" "Research" entry (file+headline "~/org/research.org" "Research"))))
