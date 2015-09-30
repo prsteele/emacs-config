b0f82f19f10eb1734477874276826c3b8bbfb857
Patrick Steele <steele.pat@gmail.com>
2015-09-29 21:55:05 -0400

    I prefer this keybinding.

--- prsteele-helm.el
+++ prsteele-helm.el
@@ -39,6 +39,8 @@
 (eval-after-load 'company
   '(progn
      (define-key company-mode-map (kbd "C-:") 'helm-company)
-     (define-key company-active-map (kbd "C-:") 'helm-company)))
+     (define-key company-active-map (kbd "C-:") 'helm-company)
+     (define-key company-mode-map (kbd "C-;") 'helm-company)
+     (define-key company-active-map (kbd "C-;") 'helm-company)))
 
 (helm-mode 1)
