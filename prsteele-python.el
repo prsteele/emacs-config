40b4f64e4c9bcb65eb62b2393c9fa6521fc5ea79
Patrick Steele <steele.pat@gmail.com>
2015-09-30 23:49:19 -0400

    Fixed imports.

--- prsteele-python.el
+++ prsteele-python.el
@@ -16,11 +16,13 @@
 
 ;; (setq elpy-rpc-python-command "python3")
 
-(define-key python-mode-map (kbd "<RET>") 'newline-and-indent)
+
 
 (require 'company-jedi)
 (add-to-list 'company-backends 'company-jedi)
 
+(eval-after-load "python"
+  '(define-key python-mode-map (kbd "<RET>") 'newline-and-indent))
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
