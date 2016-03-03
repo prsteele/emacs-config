e359a95d92a90fd07ad13e5b223d7060c50ee275
Patrick Steele <prs233@cornell.edu>
2016-03-03 15:15:40 -0500

    Fonts and jedi support

--- lisp/prsteele-python.el
+++ lisp/prsteele-python.el
@@ -13,8 +13,8 @@
 ;; Basic Jedi setup
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:environment-root "jedi")
-(setq jedi:environment-virtualenv
-      (append python-environment-virtualenv
-              '("--python" "/usr/bin/python3")))
+;; (setq jedi:environment-virtualenv
+;;       (append python-environment-virtualenv
+;;               '("--python" "/usr/bin/python3")))
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
