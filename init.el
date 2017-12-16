eea51c23e36b4db374eaf0c09d6f626676ef8758
Patrick Steele <steele.pat@gmail.com>
2017-12-16 11:41:11 -0500

    Add tramp support

--- init.el
+++ init.el
@@ -5,17 +5,28 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
- '(custom-theme-directory "~/.emacs.d/themes/")
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(ecb-options-version "2.40")
+ '(elpy-rpc-python-command "python3")
+ '(frame-background-mode (quote dark))
+ '(haskell-literate-default (quote tex))
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t))
+ '(haskell-process-suggest-remove-import-lines t)
+ '(send-mail-function (quote mailclient-send-it))
+ '(tramp-remote-path
+   (quote
+    (tramp-own-remote-path tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin" tramp-default-remote-path))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((background dark) (type graphic)) (:foreground "#839496" :background "#002b36")) (((background dark) (type tty) (min-colors 256)) (:foreground "brightblue" :background "brightblack")) (((background dark) (type tty) (min-colors 16)) (:foreground "brightblue" :background "brightblack")) (((background dark) (type tty) (min-colors 8)) (:foreground nil :background nil)) (((background light) (type graphic)) (:foreground "#657b83" :background "#fdf6e3")) (((background light) (type tty) (min-colors 256)) (:foreground "brightyellow" :background "brightwhite")) (((background light) (type tty) (min-colors 16)) (:foreground "brightyellow" :background "brightwhite")) (((background light) (type tty) (min-colors 8)) (:foreground nil :background nil)))))
+ '(default ((((background dark) (type graphic)) (:foreground "#839496" :background "#002b36")) (((background dark) (type tty) (min-colors 256)) (:foreground "brightblue" :background "brightblack")) (((background dark) (type tty) (min-colors 16)) (:foreground "brightblue" :background "brightblack")) (((background dark) (type tty) (min-colors 8)) (:foreground nil :background nil)) (((background light) (type graphic)) (:foreground "#657b83" :background "#fdf6e3")) (((background light) (type tty) (min-colors 256)) (:foreground "brightyellow" :background "brightwhite")) (((background light) (type tty) (min-colors 16)) (:foreground "brightyellow" :background "brightwhite")) (((background light) (type tty) (min-colors 8)) (:foreground nil :background nil))))
+ '(font-latex-subscript-face ((t nil)))
+ '(font-latex-superscript-face ((t nil)))
+ '(haskell-interactive-face-compile-error ((t (:foreground "#ff4b4b" :weight bold))))
+ '(org-document-title ((t (:background "#303030" :foreground "#afd7ff" :weight bold)))))
 
 ;; Pull in the system environment
 (setq exec-path (append exec-path (list (getenv "PATH"))))
@@ -58,6 +69,7 @@
 		      "prsteele-sass"
 		      "prsteele-scons"
 		      "prsteele-text"
+                      "prsteele-tramp"
 		      ))
 
 ;(load-library "/home/prsteele/Documents/Development/ampl-mode/ampl-mode")
