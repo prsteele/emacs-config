8a34111f496d5fc43bf1c5ea1a4b3a671c459c77
Patrick Steele <steele.pat@gmail.com>
2018-06-05 19:02:02 -0400

    Ignore sub- and super-scripts in LaTeX mode

--- lisp/prsteele-latex.el
+++ lisp/prsteele-latex.el
@@ -5,7 +5,8 @@
 
 ;; Turn on auto-fill-mode, flyspell-mode
 (mapc (lambda (hook) (progn (add-hook hook 'auto-fill-mode)
-			    (add-hook hook 'flyspell-mode)))
+			    (add-hook hook 'flyspell-mode)
+                            (fset 'tex-font-lock-suscript 'ignore)))
       '(LaTeX-mode-hook tex-mode-hook plain-TeX-mode-hook 
 			AmS-TeX-mode-hook ConTeXt-mode-hook 
 			Texinfo-mode-hook docTeX-mode-hook))
@@ -13,6 +14,7 @@
 ;; Disable font-locking for sub- and super-scripts in the various
 ;; AuxTeX modes
 (setq font-latex-script-display 'nil)
+(setq font-latex-fontify-script 'nil)
 (setq font-latex-fontify-sectioning 'color)
 
 (setenv "TEXINPUTS" ".:~/share//:")
