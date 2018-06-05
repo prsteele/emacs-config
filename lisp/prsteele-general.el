f0be6885b033cd8f510d0cf239e8c12c9f412610
Patrick Steele <steele.pat@gmail.com>
2018-06-05 19:00:09 -0400

    Change the default font size to 11

--- lisp/prsteele-general.el
+++ lisp/prsteele-general.el
@@ -28,6 +28,6 @@
 (setq-default indent-tabs-mode nil)
 
 ;; Ubuntu 14.04 has a miserable default font in Emacs.
-(add-to-list 'default-frame-alist '(font . "mononoki 12"))
+(add-to-list 'default-frame-alist '(font . "mononoki 11"))
 (set-frame-font "mononoki 12" nil t)
 
