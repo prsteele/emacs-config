0877d3879b1e466588ec76ea69ddf04e3f5ce4d8
Patrick Steele <steele.pat@gmail.com>
2018-09-01 13:48:39 -0400

    Clean up the init.el file

--- init.el
+++ init.el
@@ -4,13 +4,12 @@
 ;; Pull in the system environment
 (setq exec-path (append exec-path (list (getenv "PATH"))))
 
-;;; Import paths
+;; Configure the packages repositories
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
 (package-initialize)
 
 ;; Find local libraries
@@ -19,30 +18,30 @@
        (normal-top-level-add-subdirs-to-load-path))
 
 ;; Load all my configuration files
-(mapc 'load-library '("prsteele-ace-jump"
-                      "prsteele-company"
-		      "prsteele-color-themes"
-                      "prsteele-general"
-                      "prsteele-c++"
-		      "prsteele-elisp"
-		      "prsteele-haskell"
-                      "prsteele-helm"
-                      "prsteele-helm-gtags"
-		      "prsteele-latex"
-                      "prsteele-java"
-		      "prsteele-markdown"
-		      "prsteele-magit"
-                      "prsteele-mode-line"
-                      "prsteele-org"
-                      "prsteele-openwith"
-                      "prsteele-prog-mode"
-                      "prsteele-python"
-                      "prsteele-rst"
-                      "prsteele-rust"
-		      "prsteele-sass"
-                      "prsteele-sql"
-		      "prsteele-scons"
-		      "prsteele-text"
-		      ))
-
-
+(mapc
+ 'load-library
+ '("prsteele-ace-jump"
+   "prsteele-company"
+   "prsteele-color-themes"
+   "prsteele-general"
+   "prsteele-c++"
+   "prsteele-elisp"
+   "prsteele-haskell"
+   "prsteele-helm"
+   "prsteele-helm-gtags"
+   "prsteele-latex"
+   "prsteele-java"
+   "prsteele-markdown"
+   "prsteele-magit"
+   "prsteele-mode-line"
+   "prsteele-org"
+   "prsteele-openwith"
+   "prsteele-prog-mode"
+   "prsteele-python"
+   "prsteele-rst"
+   "prsteele-rust"
+   "prsteele-sass"
+   "prsteele-sql"
+   "prsteele-scons"
+   "prsteele-text"
+   ))
