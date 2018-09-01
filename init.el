bbbd0f693ea725a73bfae159659278eb54e1a28c
Patrick Steele <steele.pat@gmail.com>
2018-09-01 15:58:27 -0400

    Support safely loading libraries and conditionally configuring them

--- init.el
+++ init.el
@@ -17,6 +17,37 @@
 (progn (cd "~/.emacs.d")
        (normal-top-level-add-subdirs-to-load-path))
 
+(defun safely-require (library)
+  "A variant of `require' that won't throw an error for missing files.
+
+This function calls (require LIBRARY), but handles errors by
+logging warnings to the *Warnings* buffer. Returns 't if the
+library is loaded successfully and nil otherwise."
+  (condition-case
+      nil
+      (progn (require library) 't)
+    (file-missing
+     (display-warning
+      'init
+      (format "Cannot load library %s" library)
+      :warning
+      "*Warnings*")
+     nil)))
+
+(defmacro safely-configure (libraries &rest body)
+  "Load a library using `safely-require', and configure it on success.
+
+LIBRARIES can either be a single library name, or a list of
+library names to load.
+
+BODY will only be executed if all the requested libraries are
+loaded successfully."
+  `(let ((library-list (if (listp ,libraries)
+                           ,libraries
+                         (list ,libraries))))
+     (when (eval `(and ,@(mapcar 'safely-require library-list)))
+       ,@body)))
+
 ;; Load all my configuration files
 (mapc
  'load-library
