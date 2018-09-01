736932912a0bae1ebbf4995f60ca31166767f58b
Patrick Steele <steele.pat@gmail.com>
2018-09-01 16:01:49 -0400

    Safely configure company

--- lisp/prsteele-company.el
+++ lisp/prsteele-company.el
@@ -1,7 +1,5 @@
-(require 'company)
-
-(add-hook 'after-init-hook 'global-company-mode)
-
-(add-to-list 'company-backends 'company-c-headers)
-
-(setq company-idle-delay .2)
+(safely-configure
+ 'company
+ (add-hook 'after-init-hook 'global-company-mode)
+ (add-to-list 'company-backends 'company-c-headers)
+ (setq company-idle-delay .2))
