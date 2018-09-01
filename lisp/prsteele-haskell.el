184ecbd6af68e3f0dac935b6ea7010fc771ab075
Patrick Steele <steele.pat@gmail.com>
2018-09-01 13:23:12 -0400

    Replace haskell-mode with intero

--- lisp/prsteele-haskell.el
+++ lisp/prsteele-haskell.el
@@ -4,36 +4,5 @@
 ;;
 
 (require 'haskell-mode)
-
-(mapc '(lambda (hook)
-         (add-hook 'haskell-mode-hook hook))
-      '(interactive-haskell-mode
-        haskell-indentation-mode
-        haskell-doc-mode))
-;; Set up ghc-mod
-(let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
-  (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
-  (add-to-list 'exec-path my-cabal-path))
-
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-
-(customize-set-variable 'haskell-process-suggest-remove-import-lines t)
-(customize-set-variable 'haskell-process-auto-import-loaded-modules t)
-(customize-set-variable 'haskell-process-log t)
-
-;; Hoogle
-(define-key haskell-mode-map "\C-ch" 'haskell-hoogle)
-(setq haskell-hoogle-command "hoogle")
-
-(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
-(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-
-(define-key haskell-mode-map "\C-ch" 'haskell-hoogle)
-(setq haskell-hoogle-command "hoogle")
+(require 'intero)
+(add-hook 'haskell-mode-hook 'intero-mode)
