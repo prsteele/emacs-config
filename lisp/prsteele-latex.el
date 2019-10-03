36135df37e460eedaba512562252ad36685118d1
Patrick Steele <steele.pat@gmail.com>
2019-10-03 19:31:32 -0400

    Configure LaTeX mode

--- lisp/prsteele-latex.el
+++ lisp/prsteele-latex.el
@@ -1,17 +1,21 @@
 ;;;; LaTeX configuration
 
 ;; Turn on auto-fill-mode, flyspell-mode
-(mapc (lambda (hook) (progn (add-hook hook 'auto-fill-mode)
-			    (add-hook hook 'flyspell-mode)
-                            (fset 'tex-font-lock-suscript 'ignore)))
-      '(LaTeX-mode-hook tex-mode-hook plain-TeX-mode-hook 
-			AmS-TeX-mode-hook ConTeXt-mode-hook 
-			Texinfo-mode-hook docTeX-mode-hook))
 
-;; Disable font-locking for sub- and super-scripts in the various
-;; AuxTeX modes
-(setq font-latex-script-display 'nil)
-(setq font-latex-fontify-script 'nil)
-(setq font-latex-fontify-sectioning 'color)
+(safely-configure
+ 'tex-mode
+ (mapc (lambda (hook)
+         (add-hook hook
+                   '(lambda ()
+                      (auto-fill-mode)
+                      (flyspell-mode)
+                      (fset 'tex-font-lock-suscript 'ignore)
+                      (setq font-latex-script-display 'nil)
+                      (setq font-latex-fontify-script 'nil)
+                      (setq font-latex-fontify-sectioning 'color)
+                      (setq show-trailing-whitespace 't))))
+       '(LaTeX-mode-hook tex-mode-hook plain-TeX-mode-hook
+			 AmS-TeX-mode-hook ConTeXt-mode-hook
+			 Texinfo-mode-hook docTeX-mode-hook)))
 
 (setenv "TEXINPUTS" ".:~/share//:")
