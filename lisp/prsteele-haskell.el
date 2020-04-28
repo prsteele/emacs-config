01cef72bdc63902b7579ed5bb90c4823ddbc8d8f
Patrick Steele <steele.pat@gmail.com>
2020-04-28 18:38:59 -0400

    Remove intero, an obsolete package

--- lisp/prsteele-haskell.el
+++ lisp/prsteele-haskell.el
@@ -1,12 +1,10 @@
 ;;;; Haskell configuration
 
-(use-package intero
-  :diminish intero-mode)
+(use-package lsp-haskell
+  :hook
+  ((haskell-mode . lsp)))
 
 (use-package haskell-mode
-  :hook
-  (haskell-mode . intero-mode)
-  :config
-  (setq haskell-mode-stylish-haskell-path "stylish-haskell"))
-
-;; Use customize to set haskell-stylish-on-save to 't
+  :custom
+  (haskell-stylish-on-save t)
+  (haskell-mode-stylish-haskell-path "stylish-haskell"))
