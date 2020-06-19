6dd2ed20d901a8f208bd6afe4cde7a572c074acd
Patrick Steele <steele.pat@gmail.com>
2020-06-18 22:28:41 -0400

    Enable black on save

--- lisp/prsteele-python.el
+++ lisp/prsteele-python.el
@@ -10,3 +10,8 @@
   (python-environment-directory venv-location)
   (python-shell-interpreter "python")
   (python-shell-interpreter-args "-i"))
+
+(use-package python-black
+  :after python
+  :hook
+  ((python-mode . python-black-on-save-mode)))
