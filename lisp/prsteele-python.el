0b0c09e3256fe37743f20a307cc9c770fac6c24b
Patrick Steele <steele.pat@gmail.com>
2020-06-18 21:46:05 -0400

    Configure Python for LSP

--- lisp/prsteele-python.el
+++ lisp/prsteele-python.el
@@ -1,23 +1,12 @@
 (use-package python-environment)
 
-(use-package company-jedi
-  :after (python-environment)
-  :demand
+(use-package lsp-pyls
   :hook
-  ((python-mode . jedi:setup)
-   (python-mode . company-mode))
+  ((python-mode . lsp)))
 
+(use-package virtualenvwrapper
   :custom
   (venv-location (expand-file-name "~/.virtualenvs"))
   (python-environment-directory venv-location)
-  (jedi:environment-root "jedi")
-  (jedi:environment-virtualenv
-   (append python-environment-virtualenv
-           '("--python" "/usr/bin/python3.7")))
-  (jedi:setup-keys 't)
-  (jedi:complete-on-dot 't)
   (python-shell-interpreter "python")
-  (python-shell-interpreter-args "-i")
-
-  :config
-  (add-to-list 'company-backends 'company-jedi))
+  (python-shell-interpreter-args "-i"))
