3903ed38a1f31e66838c27a0c0420eb19e1e56c8
Patrick Steele <steele.pat@gmail.com>
2020-06-18 21:44:49 -0400

    Configure Haskell for LSP

--- lisp/prsteele-haskell.el
+++ lisp/prsteele-haskell.el
@@ -3,4 +3,11 @@
 (use-package haskell-mode
   :custom
   (haskell-stylish-on-save t)
-  (haskell-mode-stylish-haskell-path "stylish-haskell"))
+  (haskell-mode-stylish-haskell-path "stylish-haskell")
+  (haskell-process-path-stack "~/.local/bin/stack"))
+
+(use-package haskell-process)
+
+(use-package haskell-interactive-mode
+  :hook
+  (haskell-mode . interactive-haskell-mode))
