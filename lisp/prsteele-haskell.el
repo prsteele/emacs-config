e937d92f4a5e061585c98e01d860cfb0d27c83e4
Patrick Steele <steele.pat@gmail.com>
2020-11-15 19:57:33 -0500

    Upgrade Haskell

--- lisp/prsteele-haskell.el
+++ lisp/prsteele-haskell.el
@@ -1,13 +1,42 @@
 ;;;; Haskell configuration
 
 (use-package haskell-mode
+  :after (reformatter eglot lsp-haskell)
   :custom
-  (haskell-stylish-on-save t)
+  (haskell-stylish-on-save nil)
   (haskell-mode-stylish-haskell-path "stylish-haskell")
-  (haskell-process-path-stack "~/.local/bin/stack"))
+  (haskell-process-path-stack "~/.local/bin/stack")
+
+  :hook
+  ((haskell-mode . ormolu-format-on-save-mode)
+   (haskell-mode . eglot-ensure))
+
+  :config
+
+  (add-to-list 'eglot-server-programs '(haskell-mode . ("ghcide" "--lsp")))
+
+  (defvar-local ormolu-command "ormolu" "The command to run when applying ormolu formatting")
+
+  (reformatter-define ormolu-format
+    :program ormolu-command
+    :args '()
+    :lighter " ormolu")
+
+  ;; Turn off broken flymake functions
+  (setq flymake-allowed-file-name-masks
+	   (remove '("\\.l?hs\\'" haskell-flymake-init)
+		   flymake-allowed-file-name-masks)))
 
 (use-package haskell-process)
 
 (use-package haskell-interactive-mode
   :hook
-  (haskell-mode . interactive-haskell-mode))
+  (haskell-mode . interactive-haskell-mode)
+
+  :custom
+  (haskell-process-suggest-remove-import-lines nil))
+
+(use-package lsp-haskell
+ :config
+ (setq lsp-haskell-process-path-hie "ghcide")
+ (setq lsp-haskell-process-args-hie '()))
