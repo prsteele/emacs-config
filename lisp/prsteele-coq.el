d314256309cc837cf7266fbe0a03d5065b286e51
Patrick Steele <steele.pat@gmail.com>
2021-03-04 00:34:23 -0500

    Color Coq nicely

--- lisp/prsteele-coq.el
+++ lisp/prsteele-coq.el
@@ -3,5 +3,9 @@
   (:map coq-mode-map
         (("RET" . newline-and-indent)))
   :custom
-  (coq-compile-before-require . 't)
-  (proof-locked-face))
+  (coq-compile-before-require t)
+  :custom-face
+  (proof-locked-face ((t (:extend t :background "#073642"))))
+  (proof-queue-face ((t (:extend t :background "#d33682"))))
+  :hook
+  ((coq-mode . company-coq-mode)))
