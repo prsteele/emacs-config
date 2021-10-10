fc64c985179523f401fc4619f45c0e450397f35f
Patrick Steele <steele.pat@gmail.com>
2021-10-10 11:12:07 -0400

    Configure Python to use formatter.el

--- lisp/prsteele-python.el
+++ lisp/prsteele-python.el
@@ -1,3 +1,28 @@
+(defcustom prsteele-python-mode-lsp-server-path
+  "pyls"
+  "The path to the Python language server program"
+  :type 'string
+  :safe 'stringp
+  :group 'prsteele)
+
+(defun eglot-python-lsp-server-fn (was-interactive)
+  "A function to compute the LSP server for Python"
+  (list prsteele-python-mode-lsp-server-path))
+
+(defvar-local isort-command "isort" "The command to run when applying isort formatting")
+
+(reformatter-define isort-format
+  :program isort-command
+  :args '("-")
+  :lighter " isort")
+
+(defvar-local black-command "black" "The command to run when applying isort formatting")
+
+(reformatter-define black-format
+  :program black-command
+  :args '("-")
+  :lighter " black")
+
 (use-package python
   :after (reformatter eglot)
   :bind
@@ -5,14 +30,11 @@
         (("C-c C-l" . python-shell-send-buffer)))
   :hook
   ((python-mode . isort-format-on-save-mode))
+  ((python-mode . black-format-on-save-mode))
   ((python-mode . eglot-ensure))
   :config
-  (defvar-local isort-command "isort" "The command to run when applying isort formatting")
-
-  (reformatter-define isort-format
-    :program isort-command
-    :args '("-")
-    :lighter " isort"))
+  (add-to-list 'eglot-server-programs '(python-mode . eglot-python-lsp-server-fn))
+  )
 
 (use-package python-environment)
 
@@ -22,9 +44,3 @@
   (python-environment-directory venv-location)
   (python-shell-interpreter "python")
   (python-shell-interpreter-args "-i"))
-
-(use-package python-black
-  :after python
-  :diminish (python-black-on-save-mode . "black")
-  :hook
-  ((python-mode . python-black-on-save-mode)))
