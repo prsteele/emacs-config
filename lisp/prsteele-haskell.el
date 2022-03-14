51ad67bad42da104974358760cccf83958e548a4
Patrick Steele <steele.pat@gmail.com>
2022-03-14 17:51:43 -0400

    Switch from eglot to LSP mode for Haskell

--- lisp/prsteele-haskell.el
+++ lisp/prsteele-haskell.el
@@ -1,24 +1,25 @@
 ;;;; Haskell configuration
 
-(defcustom prsteele-haskell-mode-lsp-server-path
-  "haskell-language-server"
-  "The path to the Haskell language server program"
+(defcustom ormolu-command
+  "ormolu"
+  "The command to run when applying ormolu formatting"
   :type 'string
   :safe 'stringp
   :group 'prsteele)
 
-(defun eglot-haskell-lsp-server-fn (was-interactive)
-  (list prsteele-haskell-mode-lsp-server-path "--lsp"))
+(reformatter-define ormolu-format
+  :program ormolu-command
+  :args '()
+  :lighter " ormolu")
 
 (use-package haskell-mode
-  :after (eglot)
-
   :hook
-  ((haskell-mode . eglot-ensure))
+  ((haskell-mode . (lambda ()
+                     (hack-local-variables)
+                     (lsp)))
+   (haskell-mode . ormolu-format-on-save-mode))
 
   :config
-  (add-to-list 'eglot-server-programs '(haskell-mode . eglot-haskell-lsp-server-fn))
-
   (setq haskell-process-wrapper-function
         (lambda (argv) (append (list "nix-shell" "-I" "." "--command" )
                                (list (mapconcat 'identity argv " ")))))
@@ -26,7 +27,8 @@
   ;; Turn off broken flymake functions
   (setq flymake-allowed-file-name-masks
 	   (remove '("\\.l?hs\\'" haskell-flymake-init)
-		   flymake-allowed-file-name-masks)))
+		   flymake-allowed-file-name-masks))
+  )
 
 (use-package haskell-process)
 
